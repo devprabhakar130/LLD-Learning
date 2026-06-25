edef732132e45fa70884971505aef272a965737d
devprabhakar130 <prabhakar80jha@gmail.com>
2026-06-25 19:55:27 +0530

    Open close principle example

---
+++ opencloseprinciple.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// Product class representing any item in eCommerce.
+class Product {
+public:
+    string name;
+    double price;
+
+    Product(string name, double price) {
+        this->name = name;
+        this->price = price;
+    }
+};
+
+
+class ShoppingCart {
+private:
+    vector<Product*> products;
+
+public:
+    void addProduct(Product* p) { 
+        products.push_back(p);
+    }
+
+    const vector<Product*>& getProducts() { 
+        return products;
+    } 
+
+    //Calculates total price in cart.
+    double calculateTotal() {
+        double total = 0;
+        for (auto p : products) {
+            total += p->price;
+        }
+        return total;
+    }
+};
+
+
+class ShoppingCartPrinter {
+private:
+    ShoppingCart* cart; 
+
+public:
+    ShoppingCartPrinter(ShoppingCart* cart) { 
+        this->cart = cart; 
+    }
+
+    void printInvoice() {
+        cout << "Shopping Cart Invoice:\n";
+        for (auto p : cart->getProducts()) {
+            cout << p->name << " - Rs " << p->price << endl;
+        }
+        cout << "Total: Rs " << cart->calculateTotal() << endl;
+    }
+};
+
+//Abstract class
+class Persistence {
+private:
+    ShoppingCart* cart;
+
+public:
+    virtual void save(ShoppingCart* cart) = 0; 
+};
+
+class SQLPersistence : public Persistence {
+public:
+    void save(ShoppingCart* cart) override {
+        cout << "Saving shopping cart to SQL DB..." << endl;
+    }
+};
+
+class MongoPersistence : public Persistence {
+public:
+    void save(ShoppingCart* cart) override {
+        cout << "Saving shopping cart to MongoDB..." << endl;
+    }
+};
+
+class FilePersistence : public Persistence {
+public:
+    void save(ShoppingCart* cart) override {
+        cout << "Saving shopping cart to a file..." << endl;
+    }
+};
+
+int main() {
+    ShoppingCart* cart = new ShoppingCart();
+    cart->addProduct(new Product("Laptop", 50000));
+    cart->addProduct(new Product("Mouse", 2000));
+
+    ShoppingCartPrinter* printer = new ShoppingCartPrinter(cart);
+    printer->printInvoice();
+
+    Persistence* db = new SQLPersistence();
+    Persistence* mongo = new MongoPersistence();
+    Persistence* file = new FilePersistence();
+
+    db->save(cart);   
+    mongo->save(cart); 
+    file->save(cart);  
+
+    return 0;
+}
+// This is just an simple example of Open Close principle,The main aim of open close principle is thta we can ass new functionalities without modifying the code ,we can ensure extensibility ...In this code i just make one abstarct class persitance which has one pure virtual fiunction taht means that class does not knwo how to implement it ,hides that impkementation ,but should eb one behaviour ,i used virtual for runtime poylmorphism so taht obejct function should eb called ,and later i have 3 concrete clasees in which i inherits it from persistance ,by doing this i can ensure extensibiluty without modification..
